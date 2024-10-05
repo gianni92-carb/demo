d3a211f23f7fba4495c23e2723b181b2b7fdd06b
Gianni <g.carboni@jpanik.it>
2024-10-05 20:55:46 +0200

    -gcarboni-VDEV-0000: refactor

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import HelloWorld from './HelloWorld.jsx';
+import Demo from './Demo.jsx';
 
 function App() {
   return (
     <div className="App">
         Hello!!!
+        <Demo name={"GIANNI"}/>
     </div>
   );
 }
---
+++ src/Demo.jsx
@@ -0,0 +1,71 @@
+import React from 'react';
+
+const Demo = ({ name }) => {
+const array = [1,2,3,4,5];
+
+const styles = {
+    container: {
+        display: "flex",
+        justifyContent: "space-evenly"
+    }
+};
+
+/**
+    add docstring
+*/
+function manipulateFunctionArray(arr) {
+    console.log(arr);
+    arr.push(7);
+    console.log(arr);
+    arr.pop();
+    console.log(arr);
+    return (
+        console.log("FUNCTION")
+    );
+};
+
+/**
+    add docstring
+*/
+const manipulateArrowArray = (arr) => {
+    console.log(arr);
+    const newArr = arr.map(item => item * 2);
+    console.log(newArr);
+    const odd = arr.filter(item => item % 2 !== 0);
+    console.log(odd + " dispari");
+    const even = arr.filter(item => item % 2 == 0);
+    console.log(even + " pari");
+    const pippo = arr.shift();
+    console.log(pippo + " pippo");
+    console.log(arr);
+    const removed = arr.splice(0, 1);
+    console.log(removed + " removed");
+    console.log(arr + " arr");
+    const pippo3 = arr.find(item => item === 3);
+    console.log(pippo3);
+    const index = arr.findIndex(item => item === 4);
+    console.log(index)
+    return (
+        console.log("ARROW")
+    );
+};
+
+    return (
+        <>
+            <div>
+                {`${name} PIPPO3`}
+            </div>
+            <div style={styles.container}>
+                <button onClick={() => manipulateFunctionArray(array)}>
+                    CLICK FUNCTION
+                </button>
+                <button onClick={() => manipulateArrowArray(array)}>
+                    CLICK ARROW
+                </button>
+            </div>
+        </>
+    )
+}
+
+export default Demo;
+
